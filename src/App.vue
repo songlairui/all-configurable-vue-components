8e73049112136302867d18ca6ee5f209a0aa1110
songlairui <cnre@live.cn>
2019-05-18 22:58:24 +0800

    REINIT

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
+  <div id="app" class="d-f d-f-v">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div class="stage d-f df-h">
+      <nav class="sub-nav">
+        <router-view name="nav"/>
+      </nav>
+      <div class="main">
+        <router-view/>
+      </div>
+      <div class="dashboard">
+        <router-view name="dashboard"></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="less">
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.d-f {
+  display: flex;
+  &-v {
+    flex-direction: column;
   }
+}
+.df-h {
+  flex: 9;
+}
+
+nav.sub-nav,
+.main,
+.dashboard {
+  margin: 0.5em;
+  border: medium mediumseagreen double;
+  border-radius: 1em;
+  padding: 0.5em;
+}
+.sub-nav {
+  flex: 1 0 120px;
+}
+.main {
+  flex: 2 0 768px;
+}
+.dashboard {
+  flex: 1 0 768px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <HelloWorld msg="Welcome!"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
