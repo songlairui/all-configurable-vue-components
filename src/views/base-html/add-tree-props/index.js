a6ab1e70ca56ffebf10397ae717aedec4d9eaf34
songlairui <cnre@live.cn>
2019-05-19 13:57:17 +0800

    静态使用作用域数据

--- src/views/base-html/add-tree-props/index.js
+++ src/views/base-html/add-tree-props/index.js
@@ -1,5 +1,7 @@
 import { FlexBox } from '@/definitions/types'
 
+import LayoutWrapper from './layout-wrapper'
+
 export default {
   props: {
     value: null,
@@ -7,28 +9,19 @@ export default {
     layout: FlexBox
   },
   render() {
-    const renderFlex = (meta) => {
-      const d = meta.direction === 'column' ? 'd-f-v' : ''
-      const classes = ['df-h', 'd-f', d]
-      return meta.child ? (
-        <div class={classes} style="border: thin solid gray;margin: 1px;">
-          {meta.child.map(renderFlex)}
-        </div>
-      ) : (
-        // 根据元数据改为 scopeSlot
-        <div
-          class={classes.concat('f-c-c')}
-          style="border: thin solid gray;margin: 1px;"
-        >{`slot: ${meta._id}`}</div>
-      )
-    }
-
     return (
       <div class="add-slot">
         <div class="value">value: {this.value}</div>
         <div class="child">child: {this.child && this.child.join()}</div>
-        {/* {JSON.stringify(this.layout, null, 1)} */}
-        <div class="d-f">{this.layout && this.layout.map(renderFlex)}</div>
+
+        <LayoutWrapper
+          layout={this.layout}
+          scopedSlots={{
+            aa1TeF83K: (subProp) => <div> subProp: {subProp.msg} </div>
+          }}
+        >
+          <div>default slot</div>
+        </LayoutWrapper>
       </div>
     )
   }
---
+++ src/views/base-html/add-tree-props/layout-wrapper.js
@@ -0,0 +1,35 @@
+export default {
+  functional: true,
+  name: 'LayoutWrapper',
+  props: {
+    layout: null
+  },
+  render(h, context) {
+    const { layout } = context.props
+    console.info('context', context)
+
+    const renderFlex = (meta) => {
+      const d = meta.direction === 'column' ? 'd-f-v' : ''
+      const classes = ['df-h', 'd-f', d]
+      return meta.child ? (
+        <div class={classes} style="border: thin solid gray;margin: 1px;">
+          {meta.child.map(renderFlex)}
+        </div>
+      ) : (
+        // 根据元数据改为 scopeSlot
+        <div
+          class={classes.concat('f-c-c')}
+          style="border: thin solid gray;margin: 1px;"
+        >
+          {context.scopedSlots[meta._id]
+            ? context.scopedSlots[meta._id]({ msg: '作用域数据' })
+            : `slot: ${meta._id}`}
+        </div>
+      )
+    }
+
+    return (
+      <div class="layout-wrapper d-f">{layout && layout.map(renderFlex)}</div>
+    )
+  }
+}
